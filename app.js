0d714e759db6c837c2670fbfb0f750805632a3cf
GoodStyles <goodstyles1626@email.com>
2020-06-07 16:50:25 +0530

    Added Compression package and start point changed in package.json

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const kurthiesRouter = require("./routes/kurthiRoutes");
 const jeanTopsRouter = require("./routes/jeanTopsRoutes");
 const userRouter = require("./routes/userRoutes");
 const viewRouter = require("./routes/viewRoutes");
+const compression = require("compression");
 
 const app = express();
 app.set("view engine", "pug");
@@ -17,6 +18,8 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use(express.json());
+
+app.use(compression());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
